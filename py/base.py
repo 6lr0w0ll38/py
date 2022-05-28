8f39b3a431fc45f08a5b71537a9f751b5a023c65
6lr0w0ll38 <6lr0w0ll38@gmail.com>
2022-05-28 23:11:44 +0700

    add

--- py/base.py
+++ py/base.py
@@ -1,23 +1,42 @@
-##base b in dec
-#def
-def base_to_value(a, b):
-    n = 0 #n is dec
-    p = 1
-    for _ in reversed(a):
-        n += _ * p
-        p *= b
-    return n
+#n = value dec
+#b = base
+#a = value base
 
-# #horner
+# ##base b in dec
+# #use def
+# def base_to_value(a, b):
+#     n = 0 #n is dec
+#     p = 1
+#     for _ in reversed(a):
+#         n += _ * p
+#         p *= b
+#     return n
+
+# #use horner
 # def base_to_value(a,b):
 #     n = 0
 #     for _ in a:
 #         n = n * b + _
 #     return n
 
-#drive code
-print ('base: ')
+# #drive code
+# print ('base: ')
+# b = int(input())
+# print ('nb fm list:')
+# a = list(map(int, input().split()))
+# print (base_to_value(a, b))
+
+##dec in base b use horner
+def value_in_base(n, b):
+    a = []
+    while n != 0:
+        a.append(n % b)
+        n //= b
+    return a
+
+#driver code
+print ('value in dec:')
+n = int(input())
+print('base:')
 b = int(input())
-print ('nb fm list:')
-a = list(map(int, input().split()))
-print (base_to_value(a, b))
+print (list(reversed(value_in_base(n, b))))
