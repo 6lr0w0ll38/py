c2b6dc2784520e479370c733ffeb09eec2f92a24
6lr0w0ll38 <6lr0w0ll38@gmail.com>
2022-07-09 14:43:22 +0700

    add

---
+++ py/permutations.py
@@ -0,0 +1,4 @@
+# #permutations
+# ##use package
+# import itertools
+# print (list(itertools.permutations([1, 2, 3], 3)))
--- py/print.py
+++
@@ -1,8 +0,0 @@
-print ('input number of row: ')
-nor = int(input())
-for i in range (nor):
-    n = 0
-    print ('\n')
-    while n < 2 * i + 1:
-        print (i + 1, ' ', end = '')
-        n += 1
